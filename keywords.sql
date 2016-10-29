b887c2060f618c258f9913e5ef441437a23d2ff0
Jake Crawford <jake.crawford@outlook.com>
2016-10-29 13:26:00 -0600

    Update design document and include data population files

---
+++ keywords.sql
@@ -0,0 +1,6 @@
+INSERT INTO keywords (id, keyword, starttime, userID) VALUES (1, 'headphone', '10/11/2026 15:22:32', 1);
+INSERT INTO keywords (id, keyword, starttime, userID) VALUES (2, 'jack', '10/14/2026 17:10:10', 2);
+INSERT INTO keywords (id, keyword, starttime, userID) VALUES (3, 'slow', '10/15/2026 15:44:59', 3);
+INSERT INTO keywords (id, keyword, starttime, userID) VALUES (4, 'cool', '10/18/2026 19:05:01', 4);
+INSERT INTO keywords (id, keyword, starttime, userID) VALUES (5, 'expensive', '10/20/2026 08:12:44', 5);
+
